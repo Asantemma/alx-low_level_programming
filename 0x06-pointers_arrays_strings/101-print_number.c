743147bedd162cb4164d1f5c824d36d6791ff69e
Prof <asantemma7@gmail.com>
2023-03-06 00:54:44 -0800

    pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - print integer  numbers
+ * @n: is the integer parameters
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
+	return (0);
+}
+
