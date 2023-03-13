7096fcf4f6287b856f01d3caef8faaaa43e2d882
Prof <asantemma7@gmail.com>
2023-03-13 04:36:19 -0700

    prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,10 +8,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv[]);
+	int i;
 
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
 	return (0);
 }
 
