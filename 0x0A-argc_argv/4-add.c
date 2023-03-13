4ef6a1ec0e63032fbd33ee7aa6b3035147ae23fb
Prof <asantemma7@gmail.com>
2023-03-13 06:49:51 -0700

    Add all positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,13 +10,14 @@
  */
 int check_num(char *str)
 {
+	/*Declaring variables*/
 	unsigned int count;
 
 	count = 0;
-	while (count < strlen(str))
+	while (count < strlen(str)) /*count string*/
 
 	{
-		if (!isdigit(str[count]))
+		if (!isdigit(str[count])) /*check if str there are digit*/
 		{
 			return (0);
 		}
@@ -27,9 +28,9 @@ int check_num(char *str)
 }
 
 /**
- * main - Adds positive numbers
+ * main - Print the name of the program
  * @argc: Count arguments
- * @argv: Array of arguments
+ * @argv: Arguments
  *
  * Return: Always 0 (Success)
  */
@@ -37,19 +38,23 @@ int check_num(char *str)
 int main(int argc, char *argv[])
 
 {
+
+	/*Declaring variables*/
 	int count;
-	int int_str;
+	int str_to_int;
 	int sum = 0;
 
 	count = 1;
-	while (count < argc)
+	while (count < argc) /*Goes through the whole array*/
 	{
 		if (check_num(argv[count]))
 
 		{
-			int_str = atoi(argv[count]);
-			sum += int_atr;
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+			sum += str_to_int;
 		}
+
+		/*Condition if one of the number contains symbols that are not digits*/
 		else
 		{
 			printf("Error\n");
@@ -59,8 +64,7 @@ int main(int argc, char *argv[])
 		count++;
 	}
 
-	printf("%d\n", sum);
+	printf("%d\n", sum); /*print sum*/
 
 	return (0);
 }
-
