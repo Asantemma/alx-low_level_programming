b559419dcfdbb275a3bf3ba1d081f567a6c0c38f
Prof <asantemma7@gmail.com>
2023-03-16 15:32:42 -0700

    more malloc free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,4 @@
-#include <stdlib.h
+#include <stdlib.h>
 #include "main.h"
 /**
  * *malloc_checked - allocates memory using malloc
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
 
 int _putchar(char c);
