1dc179c453cd7a6ff355a8529656800a81de3539
Prof <asantemma7@gmail.com>
2023-03-20 16:08:56 -0700

    Tasks pn Preprocessors

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Prints the name of a file it is compiled from
+ * main- Prints the name of a file it is compiled from
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("%s\n", _FILENAME_);
+	printf("%s\n", _FILE_);
 	return (0);
 }
