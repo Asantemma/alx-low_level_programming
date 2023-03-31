7aaf0b714755e6e1ea852ed08618e50db1e003b0
Prof <asantemma7@gmail.com>
2023-03-31 05:53:04 -0700

    pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,18 +4,26 @@
  * @dest: input value
  * @src: input value
  *
- * Return: pointer dest
- */                                                                                                                                  
-char *_strcat(char *dest, const char *src)                                                                                            
-{                                                                                                                                    
-        int index = 0, dest_len = 0;                                                                                                 
-                                                                                                                                     
-        while (dest[index++])                                                                                                        
-                dest_len++;                                                                                                          
-                                                                                                                                     
-        for (index = 0; src[index]; index++)                                                                                         
-                dest[dest_len++] = src[index];                                                                                       
-                                                                                                                                     
-        return (dest);                                                                                                               
-}
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
 
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
