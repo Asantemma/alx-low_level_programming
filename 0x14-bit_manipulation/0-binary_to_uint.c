4f61394f35cec1e24b1d3c4ca2eaf200e9a37a28
Asante <asantemma7@gmail.com>
2023-04-09 12:50:08 +0000

    Bit Manipulation in C

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,7 +12,7 @@ unsigned int binary_to_uint(const char *b)
 int i = 0;
 unsigned int num = 0;
 
-	if (b == NULL || b[i] == '0' || b[i] == '1')
+	if (b == NULL)
 	return (0);
 
 	while (b[i] == '0' || b[i] == '1')
