0d20b91407fc8101a68c86e0eef2639d37cd801b
Asante <asantemma7@gmail.com>
2023-04-09 18:03:53 +0000

    Bit Manipulation in C

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,5 +11,6 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 int _putchar(char c);
+int get_endianness(void);
 
 #endif
