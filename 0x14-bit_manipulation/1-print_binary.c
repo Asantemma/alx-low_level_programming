67a8e85c8c49e17cf392a5adb3f64a3453768bdd
Asante <asantemma7@gmail.com>
2023-04-09 15:35:02 +0000

    Bit Manipulation in C

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - function that prints the binary representation of a number.
+ * @n: Binary number to print
+ *
+ * Return: 1 for success; -1 for failure
+ */
+
+void print_binary(unsigned long int n)
+{
+int i;
+unsigned long int count;
+
+count << "0";
+for (i = 1 << 30; i > 0; i = i / 2)
+	{
+	if((n & i) != 0)
+		{
+		count << "1";
+		}
+	else
+		{
+		count << "0";
+		}
+	}
+}
