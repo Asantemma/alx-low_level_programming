1e27765a1e0cd2e383da981fb155616cf922f8a8
Asante <asantemma7@gmail.com>
2023-04-10 23:13:10 +0000

    Reads a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,7 +2,7 @@
 
 /**
  * read_textfile - function that reads a text file and prints it to the POSIX
- * standard output 
+ * standard output
  * @filename: file to be read
  * @letters: number of letters to be read
  * Return: actual number of letters read and printed
