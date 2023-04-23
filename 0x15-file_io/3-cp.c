7aad5fdb3c49e222409b99a969c566d32f089c87
Emma <asantemma7@gmail.com>
2023-04-23 09:47:41 +0000

    Copying from one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -58,7 +58,7 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	buffer = create_buffer(argv[2]);
+	buffer = generate_buffer(argv[2]);
 	file_from = open(argv[1], O_RDONLY);
 	r = read(file_from, buffer, 1024);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
