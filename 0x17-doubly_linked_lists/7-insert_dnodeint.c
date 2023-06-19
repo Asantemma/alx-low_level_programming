f8a63d472a6970c9eea97bb2f082f22cb4eeffb7
ASANTE <asantemma7@gmail.com>
2023-06-19 09:17:55 +0000

    made changes to files

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,9 +17,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (h == NULL || new_node == NULL)
 		return (NULL);
 
-	*new_node.n = n;
-	*new_node.prev = NULL;
-	*new_node.next = NULL;
+	(*new_node).n = n;
+	(*new_node).prev = NULL;
+	(*new_node).next = NULL;
 	ptr = *h;
 	if (idx == 0)
 	{
@@ -28,22 +28,22 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	while (ptr)
 	{
-		if (*ptr.next == NULL && x == idx - 1)
+		if ((*ptr).next == NULL && x == idx - 1)
 		{
 			new_node = add_dnodeint_end(h, n);
 			return (new_node);
 		}
 		else if (x == idx)
 		{
-			*new_node.next = ptr;
-			*new_node.prev = ptr->prev;
+			(*new_node).next = ptr;
+			(*new_node).prev = ptr->prev;
 
 			ptr->prev->next = new_node;
-			*ptr.prev = new_node;
+			(*ptr).prev = new_node;
 			return (new_node);
 		}
 
-		ptr = *ptr.next;
+		ptr = (*ptr).next;
 		x++;
 	}
 
